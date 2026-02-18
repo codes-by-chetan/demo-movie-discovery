4bd7e86a502a1c69f0759730438dc26b53100ed8
bhavesh-stingrai <b.borgoankar@stingrai.io>
2026-02-18 13:14:01 +0530

    initial setup

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AppNavigator = () => {
+  return (
+    <View>
+      <Text>test app</Text>
+    </View>
+  )
+}
+
+export default AppNavigator
